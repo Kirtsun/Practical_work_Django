1d65bb2e044f39f06c9761a31e5abe077ec37432
Kirtsun <olegkirtsun@gmail.com>
2022-12-05 21:53:57 +0200

    new post

--- dz_practical/views.py
+++ dz_practical/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.utils import timezone
 
-# Create your views here.
+from dz_practical.forms import PostsForm
+
+
+def post_new(request):
+    if request.method == 'Post':
+        form = PostsForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('index')
+    else:
+        form = PostsForm()
+    return render(request, 'new_post.html', {'form': form})
