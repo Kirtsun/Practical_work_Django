bb8fd0829b08740a0c332cb42c93b60462362895
Kirtsun <olegkirtsun@gmail.com>
2022-12-15 20:47:53 +0200

    celery and cache

--- dz_practical/models.py
+++ dz_practical/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django_lifecycle import LifecycleModel, hook, AFTER_UPDATE
-from .tasks import send_mail
+from django.core.mail import send_mail
 User = get_user_model()
 
 
@@ -28,9 +28,8 @@ class Comments(LifecycleModel):
 
     @hook(AFTER_UPDATE, when="is_publish", was=False, is_now=True)
     def on_publish(self):
-        send_mail.delay(subject='You have a new Comment', text=' http://127.0.0.1:8000'
-                                                               + reverse('post_detail', args=[str(self.post.id)]),
-                        to_email=self.post.author.email)
+        send_mail('You have a new Comment', ' http://127.0.0.1:8000' + reverse('post_detail', args=[str(self.post.id)]),
+                  'compane@gmai;.com', [self.post.author.email])
 
     def __str__(self):
         return self.text
