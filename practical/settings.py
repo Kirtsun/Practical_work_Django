b23c522ede5f57fef4cc18cd2ff492f282709ce4
Kirtsun <olegkirtsun@gmail.com>
2022-12-20 22:15:35 +0200

    Deploy

--- practical/settings.py
+++ practical/settings.py
@@ -67,7 +67,6 @@ if DEBUG:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
